348e86ec81e4b2f7b29f981418d71537ca8eb511
Stephano Hernandez <stephanoah@protonmail.com>
2021-07-25 22:29:03 -0400

    Markdown is done

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -2,14 +2,19 @@
 import React from "react";
 //Components
 //Styles
-{
-  /* import styles from "./TodoCard.modules.css"; */
-}
+import styles from "./TodoCard.module.css"; 
 
 function TodoCard() {
   return (
-    <div>
-      <h1>TodoCard</h1>
+    <div className={styles.Container}>
+      <div className={styles.TextContainer}>
+        <h2 className={styles.Title}>Title</h2>
+        <p className={styles.Description}>Description</p>
+      </div>
+      <div className={styles.BtnContainer}>
+        <button type="checkbox" className={styles.DoneBtn}>Done</button>
+        <button className={styles.EditBtn}>Edit</button>
+      </div>
     </div>
   );
 }
--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,14 +1,18 @@
 import React from "react";
 //Components
 //Styles
-{
-  /* import styles from "./TodoForm.modules.css" */
-}
+import styles from "./TodoForm.module.css"
 
 function TodoForm() {
   return (
     <div>
-      <h1>TodoForm</h1>
+      <form className={styles.Form} action="">
+        <label className={styles.Label} for="Title">Title</label>
+        <input className={styles.Input} type="text" id="Title" />
+        <label className={styles.Label} for="Description">Description</label>
+        <input className={styles.Input} type="textarea" id="Description" />
+        <button className={styles.CreateBtn}>Create</button>
+      </form>
     </div>
   );
 }
